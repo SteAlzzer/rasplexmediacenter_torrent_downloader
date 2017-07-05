d4693d5215daed7e17472c7577f2fe727f36b6db
m.kruglov <m.kruglov@npo-echelon.ru>
2017-07-05 23:45:29 +0300

    prerelease

--- new_way.py
+++ new_way.py
@@ -22,8 +22,7 @@ TMP_FILE = '/tmp/tbox.tmp'
 
 ADD_NO_DEST_FILES = False
 
-DELAY = 5  # 2 min
-# DELAY = 60 * 2  # 2 min
+DELAY = 60 * 10  # 10 min
 
 
 def list_folder(folder):
@@ -36,20 +35,20 @@ def list_folder(folder):
 
 
 def dropbox_check_new_files():
-    print('~Downloading files from dropbox')
+    print('~ Downloading files from dropbox')
     dropbox_download_files()
     new_files = []
     for file in list_folder(DROPBOX_LOCAL_FOLDER):
         rel_file = os.path.relpath(file, DROPBOX_LOCAL_FOLDER)
         if rel_file not in DROPBOX_LOCAL_FILE_LIST:
-            print('~New file found', rel_file)
+            print('~ New file found', rel_file)
             new_files.append(file)
             continue
         else:
             if int(os.path.getsize(file)) != DROPBOX_LOCAL_FILE_LIST[rel_file]['size'] or int(os.path.getmtime(file)) != DROPBOX_LOCAL_FILE_LIST[rel_file]['mtime']:
-                print('~Metadata changed', file)
-                print('~ > file', int(os.path.getsize(file)), int(os.path.getmtime(file)))
-                print('~ > saved', DROPBOX_LOCAL_FILE_LIST[rel_file]['size'], DROPBOX_LOCAL_FILE_LIST[rel_file]['mtime'])
+                print('~ Metadata changed', file)
+                print('~  > file', int(os.path.getsize(file)), int(os.path.getmtime(file)))
+                print('~  > saved', DROPBOX_LOCAL_FILE_LIST[rel_file]['size'], DROPBOX_LOCAL_FILE_LIST[rel_file]['mtime'])
                 new_files.append(file)
                 continue
 
@@ -62,30 +61,30 @@ def dropbox_download_files():
 
 
 def dropbox_move_file(source_file, dest_file):
-    print('~Moving files in dropbox', source_file, dest_file)
+    print('~ Moving files in dropbox', source_file, dest_file)
     if os.path.split(source_file)[1] == os.path.split(dest_file)[1]:
-        print('~ Files have same names. Skipping...')
+        print('~  Files have same names. Skipping...')
         return
     source_file_rel = os.path.relpath(source_file, DROPBOX_LOCAL_FOLDER)
     dest_file_rel = os.path.relpath(dest_file, DROPBOX_LOCAL_FOLDER)
 
     command = '"{}" move "{}" "{}"'.format(DROPBOX_SCRIPT, source_file_rel, dest_file_rel)
-    print('~Using command', command)
+    print('~ Using command', command)
     os.system(command)
 
 
 def dropbox_update_local_filelist(file_to_update):
-    print('~Refresh monitoring files list')
+    print('~ Refresh monitoring files list')
     rel_file = os.path.relpath(file_to_update, DROPBOX_LOCAL_FOLDER)
     if rel_file not in DROPBOX_LOCAL_FILE_LIST:
-        print('~This is new file in list', rel_file)
+        print('~ This is new file in list', rel_file)
         DROPBOX_LOCAL_FILE_LIST[rel_file] = {}
     DROPBOX_LOCAL_FILE_LIST[rel_file]['size'] = int(os.path.getsize(file_to_update))
     DROPBOX_LOCAL_FILE_LIST[rel_file]['mtime'] = int(os.path.getmtime(file_to_update))
 
 
 def dropbox_save_local_filelist():
-    print('~Backuping monitoring list')
+    print('~ Backuping monitoring list')
     with open(DROPBOX_LOCAL_FILE_LIST_BACKUP, 'w') as bkp:
         for key, value in DROPBOX_LOCAL_FILE_LIST.items():
             line = '{}:{};{};\n'.format(key, value['size'], value['mtime'])
@@ -93,9 +92,9 @@ def dropbox_save_local_filelist():
 
 
 def dropbox_load_local_filelist():
-    print('~Loading monitoring backup')
+    print('~ Loading monitoring backup')
     if not os.path.isfile(DROPBOX_LOCAL_FILE_LIST_BACKUP):
-        print('~File is not exists')
+        print('~ File is not exists')
         return
 
     for line in open(DROPBOX_LOCAL_FILE_LIST_BACKUP):
@@ -106,16 +105,16 @@ def dropbox_load_local_filelist():
 
 def torrent_get_real_filename(torrent_file):
     # todo: change ">>" for PIPES
-    print('~Getting realname for torrent file', torrent_file)
+    print('~ Getting realname for torrent file', torrent_file)
     command = 'transmission-show "{}" | grep -oP "^Name:\s?\K.*" >> "{}"'.format(torrent_file, TMP_FILE)
     os.system(command)
-    print('~Command has been launched', command)
+    print('~ Command has been launched', command)
     with open(TMP_FILE) as f:
         real_name = f.readline().strip() + '.torrent'
         torrent_dir = os.path.split(torrent_file)[0]
         real_fullname = os.path.join(torrent_dir, real_name.replace(' ', '_'))
     remove_tmp_file()
-    print('~Realname got:', real_fullname)
+    print('~ Realname got:', real_fullname)
     return real_fullname
 
 
@@ -124,11 +123,11 @@ def torrent_check_daemon():
 
 
 def remove_tmp_file(tries=10):
-    print('~Removing tmp file', tries)
+    print('~ Removing tmp file', tries)
     try:
         if os.path.isfile(TMP_FILE):
             os.remove(TMP_FILE)
-            print('~Removed!')
+            print('~ Removed!')
     except:
         if tries:
             time.sleep(1)
@@ -140,13 +139,13 @@ def remove_tmp_file(tries=10):
 
 def torrent_add_file(torrent_file, dest_folder):
     command = 'transmission-remote -n "tr:tr" --add "{}" -w "{}"'.format(torrent_file, dest_folder)
-    print('~Adding torrent file to transmission with command:', command)
+    print('~ Adding torrent file to transmission with command:', command)
     os.system(command)
 
 
 def is_file_torrent(file):
     # todo: get some real shit
-    print('~ Here is an extension...', os.path.splitext(file)[1], os.path.splitext(file)[1] == '.torrent')
+    print('~  Here is an extension...', os.path.splitext(file)[1], os.path.splitext(file)[1] == '.torrent')
     return os.path.splitext(file)[1] == '.torrent'
 
 
@@ -155,23 +154,23 @@ def is_file_cmd(file):
 
 
 def rename_file(orig_file, dest_file):
-    print('~Renaming local file', orig_file, dest_file)
+    print('~ Renaming local file', orig_file, dest_file)
     os.rename(orig_file, dest_file)
 
 
 def get_dest_path_for_torrent_file(torrent_file):
-    print('~Getting destination folder for file')
+    print('~ Getting destination folder for file')
     dropbox_torrent_folder_path = os.path.join(DROPBOX_LOCAL_FOLDER, DROPBOX_TORRENT_FOLDER)
     torrent_file_rel = os.path.relpath(torrent_file, dropbox_torrent_folder_path)
-    print('~Related path for file:', torrent_file_rel)
+    print('~ Related path for file:', torrent_file_rel)
     for folder_name in FOLDERS_MAP_STRUCTURE.keys():
         if folder_name in torrent_file_rel:
-            print('~This is what we have got', FOLDERS_MAP_STRUCTURE[folder_name])
+            print('~ This is what we have got', FOLDERS_MAP_STRUCTURE[folder_name])
             return FOLDERS_MAP_STRUCTURE[folder_name]
 
 
 def main_cycle():
-    print('~mainCycle is launched')
+    print('~ mainCycle is launched')
     while True:
         new_files = dropbox_check_new_files()
 
@@ -192,12 +191,12 @@ def main_cycle():
 
                 dropbox_update_local_filelist(original_file)
             dropbox_save_local_filelist()
-        print('~Sleep')
+        print('~ Sleep')
         time.sleep(DELAY)
 
 
 def main():
-    print('~Lets go')
+    print('~ Lets go')
     remove_tmp_file()
     torrent_check_daemon()
     dropbox_load_local_filelist()
